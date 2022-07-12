061d6a58640eca7643f1799a65dae9f6a25970ea
Alejandro <albernalca@gmail.com>
2022-07-12 06:45:18 -0500

    se pasan datos a la vista con map

--- src/main/java/com/apirest/controllers/IndexController.java
+++ src/main/java/com/apirest/controllers/IndexController.java
@@ -1,5 +1,7 @@
 package com.apirest.controllers;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -8,8 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class IndexController {
 	
 	@GetMapping({"/index","/","/home"})
-	public String index() {
-		
+	public String index(Map<String, Object> map) {
+		map.put("titulo", "Spring Framework");
 		return "index";
 		
 	}
