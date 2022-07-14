1e8cd4b06e99e9fa57e70168e1e2b6c42e33652e
Alejandro <albernalca@gmail.com>
2022-07-14 17:03:22 -0500

    se realizan cambios en el index

--- src/main/java/com/apirest/controllers/IndexController.java
+++ src/main/java/com/apirest/controllers/IndexController.java
@@ -4,9 +4,11 @@ import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
+@RequestMapping("/app")
 public class IndexController {
 	
 	@GetMapping({"/index","/","/home"})
