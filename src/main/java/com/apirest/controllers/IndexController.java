98e2a0a7ca3eab3f19f8bc59907d8bf392be913d
Your Name <you@example.com>
2022-07-30 17:42:24 -0500

    Se crea controlador y vista con el objeto Model

--- src/main/java/com/apirest/controllers/IndexController.java
+++ src/main/java/com/apirest/controllers/IndexController.java
@@ -1,20 +1,36 @@
 package com.apirest.controllers;
 
-import java.util.Map;
-
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.apirest.models.Usuario;
 
 
 @Controller
 @RequestMapping("/app")
 public class IndexController {
 	
-	@GetMapping({"/index","/","/home"})
-	public String index(Map<String, Object> map) {
-		map.put("titulo", "Spring Framework");
+	@GetMapping({"/index", "/", "", "/home"})
+	public String index(Model model) {
+		model.addAttribute("titulo", "Spring Framework");
 		return "index";
+		
+	}
+	
+	@RequestMapping("/perfil")
+	public String perfil(Model model) {
+		
+		Usuario usuario = new Usuario();
+		
+		usuario.setNombre("Alejandro");
+		usuario.setApellido("Bernal Castiblanco");
+		
+		model.addAttribute("usuario", usuario);
+		model.addAttribute("titulo", "Perfil Usuario: ".concat(usuario.getNombre()));
+		
+		return "perfil";
 		
 	}
 }
---
+++ src/main/java/com/apirest/models/Usuario.java
@@ -0,0 +1,24 @@
+package com.apirest.models;
+
+public class Usuario {
+	
+	public String nombre;
+	public String apellido;
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	
+	
+	
+
+}
